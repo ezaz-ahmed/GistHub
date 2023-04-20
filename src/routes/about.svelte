08c5c02821ee38f777cd688366d89bf1b7179b7b
Ezaz Ahmed <ezaznahmed@gmail.com>
2023-04-20 13:45:34 +0600

    Initial Commit

--- src/routes/about.svelte
+++
@@ -1,7 +0,0 @@
-<svelte:head>
-	<title>About</title>
-</svelte:head>
-
-<h1 class="text-4xl text-center my-8 uppercase">
-	Created With Love 💓 By <a href="https://github.com/ezaz-ahmed">Ezaz Ahmed</a>
-</h1>
