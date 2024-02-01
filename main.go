f5e29c64769212f0539cf7774a65398e1da5b469
Ezaz Ahmed <ezaznahmed@gmail.com>
2024-02-01 23:25:23 +0600

    :white_check_mark: different constructur for filemanager

--- main.go
+++ main.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/ezaz-ahmed/money-minder/filemanager"
+	"github.com/ezaz-ahmed/money-minder/cmdmanager"
 	"github.com/ezaz-ahmed/money-minder/prices"
 )
 
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
-
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		priceJob := prices.NewTextIncludedPriceJob(fm, taxRate)
+		cmdm := cmdmanager.New()
+
+		priceJob := prices.NewTextIncludedPriceJob(cmdm, taxRate)
 		priceJob.Process()
 	}
 }
--- prices/prices.go
+++ prices/prices.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 
 	"github.com/ezaz-ahmed/money-minder/conversion"
-	"github.com/ezaz-ahmed/money-minder/filemanager"
+	"github.com/ezaz-ahmed/money-minder/iomanager"
 )
 
 type TextIncludedPriceJob struct {
-	IOManager          filemanager.FileManager `json:"-"`
-	TaxRate            float64                 `json:"tax_rate"`
-	InputPrices        []float64               `json:"input_prices"`
-	TextIncludedPrices map[string]string       `json:"tax_included_prices"`
+	IOManager          iomanager.IOManager `json:"-"`
+	TaxRate            float64             `json:"tax_rate"`
+	InputPrices        []float64           `json:"input_prices"`
+	TextIncludedPrices map[string]string   `json:"tax_included_prices"`
 }
 
 func (job *TextIncludedPriceJob) LoadPrices() {
@@ -45,9 +45,9 @@ func (job *TextIncludedPriceJob) Process() {
 	job.IOManager.WriteResult(job)
 }
 
-func NewTextIncludedPriceJob(fm filemanager.FileManager, taxRate float64) *TextIncludedPriceJob {
+func NewTextIncludedPriceJob(iom iomanager.IOManager, taxRate float64) *TextIncludedPriceJob {
 	return &TextIncludedPriceJob{
-		IOManager:   fm,
+		IOManager:   iom,
 		InputPrices: []float64{10, 20, 30},
 		TaxRate:     taxRate,
 	}
